1b5035067e7977cae5a34e0fa2151f1d45b6e7e0
Signor Koala <signor-koala@signor-koala.dev>
2025-08-25 13:44:27 +0530

    yubikey

---
+++ modules/system/security.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  services.udev.packages = [pkgs.yubikey-personalization];
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+}
