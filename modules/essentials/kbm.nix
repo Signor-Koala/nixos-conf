6b78b3b244bfed6a85d75c29b69b94573ccdc556
Signor Koala <signor-koala@signor-koala.dev>
2025-10-08 08:13:29 +0530

    Replaced itools with keyd for capslock remapping Because itools failed to build and I am too lazy to wait for it to fix itself

--- modules/essentials/kbm.nix
+++ modules/essentials/kbm.nix
@@ -4,17 +4,19 @@
   pkgs,
   ...
 }: {
-  services.interception-tools = let
-    itools = pkgs.interception-tools;
-    itools-caps = pkgs.interception-tools-plugins.caps2esc;
-  in {
+  services.keyd = {
     enable = true;
-    plugins = [itools-caps];
-    udevmonConfig = pkgs.lib.mkDefault ''
-      - JOB: "${itools}/bin/intercept -g $DEVNODE | ${itools-caps}/bin/caps2esc | ${itools}/bin/uinput -d $DEVNODE"
-        DEVICE:
-          EVENTS:
-            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
-    '';
+    keyboards = {
+      default = {
+        ids = ["*"];
+        settings = {
+          main = {
+            capslock = "overload(control, esc)";
+            esc = "capslock";
+          };
+          otherLayer = {};
+        };
+      };
+    };
   };
 }
